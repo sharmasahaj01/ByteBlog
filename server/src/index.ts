b8e3ffe5b642149c9d9cf2ada0c67893cea61e4b
Sahaj <sahajs5685@gmail.com>
2024-03-07 22:13:29 +0530

    sigin,signup route and jwt auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,70 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 
 const app = new Hono<{
   Bindings:{
     DATABASE_URL:string;
+    JWT_SECRET:string;
   }
 }>()
 
-app.post('/api/v1/user/signup', (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+  const body = await c.req.json()
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  return c.text("Signed Up Successfully")
+
+  try {
+    const user = await prisma.user.create({
+      data:{
+        username:body.username,
+        password:body.password,
+        name:body.name
+      }
+    })
+    const jwt = await sign({
+      id:user.id
+    },c.env.JWT_SECRET)
+
+    return c.text(jwt)
+  } catch (e) {
+    c.status(411)
+    return c.text("Invalid")
+  }
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signin', async (c) => {
+  const body = await c.req.json()
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+
+  try {
+    const user = await prisma.user.findUnique({
+      where:{
+        username:body.username,
+        password:body.password,
+      }
+    })
+    if(!user){
+      c.status(403)
+      c.text("Invalid credentials")
+    }
+    const jwt = await sign({
+      id:user?.id
+    },c.env.JWT_SECRET)
+
+    return c.text(jwt)
+  } catch (e) {
+    c.status(411)
+    return c.text("Invalid")
+  }
+
 })
 
 app.post('/api/v1/blog', (c) => {
@@ -26,10 +72,6 @@ app.post('/api/v1/blog', (c) => {
 })
 
 app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.post('/api/v1/user/signup', (c) => {
   return c.text('Hello Hono!')
 })
 
